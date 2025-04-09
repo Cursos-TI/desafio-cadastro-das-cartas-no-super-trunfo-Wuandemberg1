2c26c3f9ce2e52e5f71842d60a7714b954c080bb
Wuandemberg1 <wuandemberg@gmail.com>
2025-04-09 18:20:56 +0000

    Projeto super trunfo concluído

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,19 +5,22 @@
 int main() {
 
 
-    // Estado 1
+    // Carta 1
 
-    char codigo [20]; // Declara uma variável para armazenar o código da cidade
-    char nome[50]; // Declara uma variável para armazenar o nome do estado
+    char estado[50]; // Declara uma variável para armazenar o nome do estado
+    char codigo [20]; // Declara uma variável para armazenar o código da carta
+    char cidade[50]; // Declara uma variável para armazenar o nome da cidade
     int populacao; // Declara uma variável para armazenar a população da cidade
     float area; // Declara uma variável para armazenar a área da cidade
     float pib; // Declara uma variável para armazenar o PIB da cidade
     int pontos; // Declara uma variável para armazenar o número de pontos turísticos da cidade
 
-    printf("Digite o código da cidade\n"); // descrição do que o usuário deve fazer
-    scanf("%s", codigo); // lê o código da cidade
     printf("Digite o nome do estado\n"); // descrição do que o usuário deve fazer
-    scanf("%s", nome); // lê o nome do estado
+    scanf("%s", estado); // lê o nome do estado
+    printf("Digite o código da carta\n"); // descrição do que o usuário deve fazer
+    scanf("%s", codigo); // lê o código da carta
+    printf("Digite o nome da cidade\n"); // descrição do que o usuário deve fazer
+    scanf("%s", cidade); // lé o nome da cidade
     printf("Digite a população\n"); //descrição do que o usuário deve fazer
     scanf("%d", &populacao); // lê a população
     printf("Digite a área\n");// descrição do que o usuário deve fazer
@@ -28,9 +31,9 @@ int main() {
     scanf("%d", &pontos); // lê o número de pontos turísticos
     printf(("\n")); // pula uma linha
    
-   
-    printf("O código da cidade é: %s\n", codigo); // Exibe o código da cidade
-    printf("O nome do estado é: %s\n", nome); // Exibe o nome do estado
+    printf("O nome do estado é: %s\n", estado); // Exibe o nome do estado
+    printf("O código da carta é: %s\n", codigo); // Exibe o código da carta
+    printf("O nome da cidade é: %s\n", cidade); // Exibe o nome da cidade
     printf("A população é: %d\n", populacao); // Exibe a população
     printf("Á area é: %.2f\n", area); // Exibe a área
     printf("O pib é: %.2f\n", pib); // Exibe o PIB
@@ -43,19 +46,22 @@ int main() {
 
 
 
-    //Estado 2
+    // Carta 2
 
-    char codigo2 [20]; // Declara uma variável para armazenar o código da cidade
-    char nome2[50]; // Declara uma variável para armazenar o nome do estado
+    char estado2[50]; // Declara uma variável para armazenar o nome do estado
+    char codigo2 [20]; // Declara uma variável para armazenar o código da carta
+    char cidade2[50]; // Declara uma variável para armazenar o nome da cidade
     int populacao2; // Declara uma variável para armazenar a população da cidade
     float area2; // Declara uma variável para armazenar a área da cidade
     float pib2; // Declara uma variável para armazenar o PIB da cidade
     int pontos2; // Declara uma variável para armazenar o número de pontos turísticos da cidade
 
-    printf("Digite o código da cidade\n"); // descrição do que o usuário deve fazer
-    scanf("%s", codigo2); // lê o código da cidade
     printf("Digite o nome do estado\n"); // descrição do que o usuário deve fazer
-    scanf("%s", nome2); // lê o nome do estado
+    scanf("%s", estado2); // lê o nome do estado
+    printf("Digite o código da carta\n"); // descrição do que o usuário deve fazer
+    scanf("%s", codigo2); // lê o código da carta
+    printf("Digite o nome da cidade\n"); // descrição do que o usuário deve fazer
+    scanf("%s", cidade2); // lé o nome da cidade
     printf("Digite a população\n"); //descrição do que o usuário deve fazer
     scanf("%d", &populacao2); // lê a população
     printf("Digite a área\n");// descrição do que o usuário deve fazer
@@ -67,16 +73,18 @@ int main() {
     printf(("\n")); // pula uma linha
     
    
-    printf("O código da cidade é: %s\n", codigo2); // Exibe o código da cidade
-    printf("O nome do estado é: %s\n", nome2); // Exibe o nome do estado
+    printf("O nome do estado é: %s\n", estado2); // Exibe o nome do estado
+    printf("O código da carta é: %s\n", codigo2); // Exibe o código da carta
+    printf("O nome da cidade é: %s\n", cidade2); // Exibe o nome da cidade
     printf("A população é: %d\n", populacao2); // Exibe a população
     printf("Á area é: %.2f\n", area2); // Exibe a área
     printf("O pib é: %.2f\n", pib2); // Exibe o PIB
     printf("O número de pontos turísticos é: %d\n", pontos2); // Exibe o número de pontos turísticos
+    printf(("\n")); // pula uma linha
 
 
 
-    
+
 
     return 0;
 }
